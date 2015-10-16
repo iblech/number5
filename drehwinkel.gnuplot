7bc5108642d1e0ab59e78e304e102af29d6443e6
Ingo Blechschmidt <iblech@web.de>
2015-10-16 10:07:33 +0200

    Radialsteuerung korrigiert

---
+++ drehwinkel.gnuplot
@@ -0,0 +1,15 @@
+# N vorher definieren, zum Beispiel auf 500.
+
+plot "<./drehwinkel 0.6180339887498948482 | head -n " . N ps 1 pt 7
+pause 1
+
+plot "<./drehwinkel 0.6                   | head -n " . N ps 1 pt 7
+pause 1
+
+plot "<./drehwinkel 0.62                  | head -n " . N ps 1 pt 7
+pause 1
+
+plot "<./drehwinkel 0.618                 | head -n " . N ps 1 pt 7
+pause 1
+
+plot "<./drehwinkel 0.61803               | head -n " . N ps 1 pt 7
--- drehwinkel.hs
+++ drehwinkel.hs
@@ -4,13 +4,13 @@ import Control.Monad
 import System.Environment
 import Text.Printf
 
-points phi = go 0
-    where
-    go n = (r * cos (n*phi), r * sin (n*phi)) : go (n+1) where r = n
-
 main = do
-    [phi] <- getArgs
-    mapM_ (\(x,y) -> printf "%f\t%f\n" x y) $ points (pi * (read phi :: Double))
+    [phi] <- liftM (map read) getArgs
+    forM_ [0::Double,phi..] $ \n -> do
+        let r = n / phi
+        let x = r * cos (n*2*pi*phi)
+        let y = r * sin (n*2*pi*phi)
+        printf "%f\t%f\n" x y
 
 -- Gnuplot:
--- plot "<./drehwinkel 1.61803398874989484820 | head -n 50000" ps 1 pt 5
+-- plot "<./drehwinkel 0.61803398874989484820 | head -n 5000" ps 1 pt 5
