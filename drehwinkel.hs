1804a7f8face1c436e18f7a9a8429c1469b689a4
Ingo Blechschmidt <iblech@web.de>
2016-01-02 19:58:15 +0100

    Use complex numbers for efficiency

--- drehwinkel.hs
+++ drehwinkel.hs
@@ -2,15 +2,18 @@ module Main where
 
 import Control.Monad
 import System.Environment
-import Text.Printf
+import Data.Complex
+
+points :: Float -> [Complex Float]
+points phi = go (0::Int) 1
+    where
+    go n z = (fromIntegral n * z) : go (n+1) (z * z0)
+    z0     = cis (2 * pi * phi)
 
 main = do
     [phi] <- liftM (map read) getArgs
-    forM_ [(0::Double)..] $ \n -> do
-        let r = n
-        let x = r * cos (n*2*pi*phi)
-        let y = r * sin (n*2*pi*phi)
-        printf "%f\t%f\n" x y
+    forM_ (points phi) $ \z -> do
+        putStrLn $ show (realPart z) ++ "\t" ++ show (imagPart z)
 
 -- Gnuplot:
 -- plot "<./drehwinkel 0.61803398874989484820 | head -n 5000" ps 1 pt 5
